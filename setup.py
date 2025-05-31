d7fc92f3cdd4103a457cd8c46dd4773f362c4131
Paolo Grossi <34324085+thebitdoctor@users.noreply.github.com>
2025-05-31 18:44:04 +0200

    Update setup.py try fix

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import subprocess
-from datetime import datetime, timezone
+#from datetime import datetime, timezone
 from pathlib import Path
+#import pytz
 
 from setuptools import find_packages, setup
 
@@ -10,21 +11,20 @@ def shell(*args):
     return out.decode("ascii").strip()
 
 
-def write_version(version_core, pre_release=True):
-    if pre_release:
-        last_commit_time = shell("git", "log", "-1", "--format=%cd", "--date=iso-strict")
-        last_commit_time = datetime.strptime(last_commit_time, "%Y-%m-%dT%H:%M:%S%z")
-        last_commit_time = last_commit_time.astimezone(timezone.utc)
-        last_commit_time = last_commit_time.strftime("%y%m%d%H%M%S")
-        #version = f"{version_core}-dev{last_commit_time}"
-        version = 1
-    else:
-        version = version_core
-
-    with open(Path("resemble_enhance", "version.py"), "w") as f:
-        f.write('__version__ = "{}"\n'.format(version))
-
-    return version
+#def write_version(version_core, pre_release=True):
+#    if pre_release:
+#        last_commit_time = shell("git", "log", "-1", "--format=%cd", "--date=iso-strict")
+#        last_commit_time = datetime.strptime(last_commit_time, "%Y-%m-%dT%H:%M:%S%z")
+#        last_commit_time = last_commit_time.astimezone(timezone.utc)
+#        last_commit_time = last_commit_time.strftime("%y%m%d%H%M%S")
+#        version = f"{version_core}-dev{last_commit_time}"
+#    else:
+#        version = version_core
+#
+#    with open(Path("resemble_enhance", "version.py"), "w") as f:
+#        f.write('__version__ = "{}"\n'.format(version))
+#
+#    return version
 
 
 with open("README.md", "r") as f:
@@ -37,7 +37,8 @@ with open("requirements.txt", "r") as f:
 setup(
     name="resemble-enhance",
     python_requires=">=3.10",
-    version=write_version("0.0.2", pre_release=True),
+#    version=write_version("0.0.2", pre_release=True),
+    version=1
     description="Speech denoising and enhancement with deep learning",
     long_description=long_description,
     long_description_content_type="text/markdown",
